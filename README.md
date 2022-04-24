30c2c15e98cde138c9a3a1335a4f249effb7ec88
Gisuperu <gisubright@gmail.com>
2022-04-24 16:21:25 +0900

    make def_reader()

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # instant_quiz
+
+
+## import csvファイルフォーマット
+1. ヘッダーレコード
+`string`
+基本的に区切りを示す用．
+
+2. 基本レコード(quizを記述する)
+`idx,type,…`
+idx: 問題番号(uniqueが望ましい)
+type: quiz形式を示す，その後の形式もこれに依存する．
+
+### nsフォルダ内のtype一覧
+- select: n択空所補充，レコード`idx\t,select\t,問題,選択肢数,選択肢,…,正解番号`
+- sort: 語句整序，レコード`idx\t,sort\t,問題,語句数,語句,…,正解文字列`
+- equal: 連立完成，レコード`idx\t,equal\t,問題a,問題b,正解文字列`
+- tof: 正誤指摘，レコード`idx\t,tof\t,問題,指摘語句数,指摘語句,…,正解番号,正解文字列`
+- fill: 適語補充，レコード`idx\t,select\t,問題,空所数,正解,…`
--- trans_json.py
+++ trans_json.py
@@ -1,8 +1,37 @@
 #!/usr/bin//env Python3
 
+import json
+import csv
+
+
+filename = "ns/range1"
+
+importFile = "./org/" + filename + ".csv"
+exportFile = "./json/" + filename +".json"
+
+def reader(data): 
+    # data : list型，追記されるだけ
+    # importFileからデータを抽出する
+    with open(importFile, "r") as imf:
+        variety = "undefined"
+        for dataline in imf:
+            line = [str(s.strip()) for s in dataline.split(',')]
+            if len(line)==0:
+                variety = line[0]
+            else:
+                data.append(line.append(variety))
+
+def writer(data):
+    # すでにjsonファイルが生成済みの場合，複製(.sample)を作成する
+    print("hoge")
+
+    # 辞書型dataからjsonファイルをexportFileに出力する
+    print("hoge")
 
 def main():
-    print("hello")
+    data = reader([])
+    writer(data)
+    print("finish")
 
 if __name__=="__main__":
     main()
